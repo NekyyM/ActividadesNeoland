2eef26141eec40b1344860708e67370c789f55df
NekyyM <nekyy_@hotmail.com>
2024-11-15 12:17:39 +0100

    updateCVToony

--- MODULO 3 React/Ejercicios/MyCV/src/components/More.jsx
+++ MODULO 3 React/Ejercicios/MyCV/src/components/More.jsx
@@ -3,26 +3,28 @@ import "./More.css";
 const More = ({ volunteer, habilities }) => {
   return (
     <>
-      <div className="volunteer card">
-        {volunteer.map((item) => {
-          return (
-            <div key={JSON.stringify(item)}>
-              <p className="name">🖌 {item.name}</p>
-              <p>{item.where}</p>
-              <p>{item.description}</p>
-            </div>
-          );
-        })}
-      </div>
-      <div className="habilities card">
-        {habilities.map((item) => {
-          return (
-            <div key={JSON.stringify(item)}>
-              <p className="name">📁 {item}</p>
-            </div>
-          );
-        })}
-      </div>
+      <section className="conjuntocartas">
+        <div className="volunteer card">
+          {volunteer.map((item) => {
+            return (
+              <div key={JSON.stringify(item)}>
+                <p className="name">🖌 {item.name}</p>
+                <p>{item.where}</p>
+                <p>{item.description}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="habilities card">
+          {habilities.map((item) => {
+            return (
+              <div key={JSON.stringify(item)}>
+                <p className="name">📁 {item}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 };
